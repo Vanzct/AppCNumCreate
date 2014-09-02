8f8aef9d3d1eb3ab7b15f63ab967d77e3d906563
Vanzct <heidazhangchengtao@163.com>
2014-09-02 10:03:45 +0800

    第一次提交
    增加了一些需要的css和js还有PHP

---
+++ config.php
@@ -0,0 +1,4 @@
+<?php
+ #数据库配置
+ define("DB_DSN_SVN","mysql:host=10.1.11.63:3310;dbname=channel");
+?>
---
+++ index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	
+	<title>质量部渠道号系统</title>
+	
+
+	<style>.file-input-wrapper { overflow: hidden; position: relative; cursor: pointer; z-index: 1; }
+	.file-input-wrapper input[type=file], .file-input-wrapper input[type=file]:focus, 
+	.file-input-wrapper input[type=file]:hover { position: absolute; top: 0; left: 0; cursor: pointer; opacity: 0; filter: alpha(opacity=0); z-index: 99; outline: 0; }
+	.file-input-name { margin-left: 8px; }
+	
+	</style>
+	<link rel="stylesheet" href="js/jquery-ui/css/no-theme/jquery-ui-1.10.3.custom.min.css" id="style-resource-1">
+	<link rel="stylesheet" href="css/font-icons/entypo/css/entypo.css" id="style-resource-2">
+	<link rel="stylesheet" href="css/bootstrap-min.css" id="style-resource-4">
+	<link rel="stylesheet" href="css/neon-core-min.css" id="style-resource-5">
+	<link rel="stylesheet" href="css/neon-theme-min.css" id="style-resource-6">
+	<link rel="stylesheet" href="css/neon-forms-min.css" id="style-resource-7">
+	
+	
+	<script type=text/javascript src="js/jquery-2.1.0.min.js"></script>
+ 
+</head>
+
+<body class="page-body loaded" >
+<div class="page-container">
+	<div style="min-height: 1442px;" class="main-content">
+		页面内容写在这里
+	</div>
+</div>	
+<script src="js/highcharts.js"></script>
+<script src="js/gsap/main-gsap.js"></script>
+<script src="js/jquery-ui/js/jquery-ui-1.10.3.minimal.min.js" id="script-resource-2"></script>
+<script src="js/bootstrap.js"></script>
+
+<script src="js/joinable.js" id="script-resource-4"></script>
+<script src="js/resizeable.js"></script>  
+<script src="js/neon-api.js" id="script-resource-6"></script>
+<script src="js/cookies.min.js" id="script-resource-7"></script>
+<script src="js/jquery.validate.min.js" id="script-resource-8"></script>
+
+<script src="js/neon-login.js" id="script-resource-9"></script>
+<script src="js/neon-custom.js" id="script-resource-3"></script>
+<script src="js/neon-demo.js" id="script-resource-11"></script>
+<script src="js/neon-skins.js" id="script-resource-12"></script>
+
+<script src="js/jquery.sparkline.min.js"></script>
+<script src="js/jquery.dataTables.js"></script>
+
+<script src="js/myjs/mail.js"></script>
+<script>
+
+jQuery.noConflict();
+(function($){	
+		
+})(jQuery);
+
+
+</script>
+
+</body>
+
